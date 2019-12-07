071d992f934846eda0bceca5668b15f0b10c2721
chibuokem Jerry <profkiti@gmail.com>
2019-12-07 16:49:32 +0100

    authorization and authentication ready

--- models/user.js
+++ models/user.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
     userName: { type: String, trim : true, require : true},
     email: { type: String, required: true, lowercase: true, index: {unique: true, dropDups: true}},
-    password: { type: String, trim : true, require : true }
+    password: { type: String, trim : true, require : true },
+    createdDate: { type: Date, default: new Date() }
 })
 
 module.exports = mongoose.model('User', userSchema);
